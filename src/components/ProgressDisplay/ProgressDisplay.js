64abbc8da7fb12ef83511d49cc9b8b9d942ca77a
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2020-07-04 14:38:47 +0200

    switched places on score and difficulty

--- src/components/ProgressDisplay/ProgressDisplay.js
+++ src/components/ProgressDisplay/ProgressDisplay.js
@@ -5,8 +5,8 @@ import AuxContainer from '../../hoc/AuxContainer/AuxContainer'
 const ProgressDisplay = (props) => {
     return (
         <AuxContainer>
-            <div className={classes.ProgressDisplay}>Difficulty: {props.difficulty}</div>
             <div className={classes.ProgressDisplay}>Score: {props.score}/{props.questionNumber}</div>
+            <div className={classes.ProgressDisplay}>Difficulty: {props.difficulty}</div>
         </AuxContainer>
 
     )
